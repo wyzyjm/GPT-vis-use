6a451ac0b527e31206bdde495ef2dcc6ab509c10
v_wangyongzhi <v_wangyongzhi>
2024-11-15 17:04:45 +0800

    feat: 自定义code 和 扩展一样

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Gpt from '@/features/antv/Gpt-vis'
 import GptVisExtend from '@/features/antv/Gpt-vis-extend'
+import GptVisCode from '@/features/antv/Gpt-vis-code'
 
 function App() {
   return (
     <div>
-      <Gpt />
-      <hr />
-      <GptVisExtend />
+      {/* <Gpt /> */}
+      {/* <GptVisExtend /> */}
+      {/* 自定义 代码code */}
+      <GptVisCode />
     </div>
   )
 }
